c1b4637dcbf36e0e3ec69aafe9cf5b13528314be
rleathart <56587304+rleathart@users.noreply.github.com>
2021-06-24 21:16:00 +0100

    Refactor board_get_moves
    Closes #1
    
    Cleaned up some things:
    
    - Don't need to use a dynamic array since there can be at most 64 moves
      for any given board state
    - We can combine the code for all the sliding pieces by giving different
      start/end indexes for the slide_offset array depending on the piece
    - Clean up pawn code a little
    - Fixed pawn board wrapping bug
    - Removed janky macros

--- src/board.c
+++ src/board.c
@@ -1,6 +1,6 @@
 #include <chess/board.h>
-#include <chess/util.h>
 #include <chess/search.h>
+#include <chess/util.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,13 +39,14 @@ void board_update(Board* board, Move* move)
   board->state[move->from] = ChessPieceNone;
 }
 
-Move *board_calculate_line(Board board, int depth, bool maximising_player)
+Move* board_calculate_line(Board board, int depth, bool maximising_player)
 {
   Move best_move;
   Move* line = calloc(depth, sizeof(Move));
   for (int i = 0; i < depth; i++)
   {
-    minimax(board, depth - i, -INT_MAX, INT_MAX, maximising_player, &best_move, NULL);
+    minimax(board, depth - i, -INT_MAX, INT_MAX, maximising_player, &best_move,
+            NULL);
     board_update(&board, &best_move);
     line[i] = best_move;
     maximising_player = !maximising_player;
@@ -53,33 +54,20 @@ Move *board_calculate_line(Board board, int depth, bool maximising_player)
   return line;
 }
 
-// Verifies if the target square is either empty or contains a piece of the
-// opponent's colour
-bool board_can_move(ChessPiece piece, Board board, int pos)
+// Checks that we're not doing a self capture.
+// NOTE: Does not check for off-board wrapping
+bool board_can_capture_or_move(Board board, int origin_pos, int target_pos)
 {
-  int pos88 = topos88(pos);
-  if (pos88 & 0x88) // We're off the board
-    return false;
-  if (!board.state[pos])
+  if (board.state[target_pos] == ChessPieceNone)
     return true;
-  return (piece & ChessPieceIsWhite) ^ (board.state[pos] & ChessPieceIsWhite);
-}
-
-static void add_to_moves(Move** moves, Move move, size_t* current_alloc,
-                         size_t* idx)
-{
-  (*moves)[(*idx)++] = move;
-  if (*idx == *current_alloc)
-  {
-    *current_alloc *= 2;
-    *moves = realloc(*moves, *current_alloc);
-  }
+  return (board.state[origin_pos] & ChessPieceIsWhite) ^
+         (board.state[target_pos] & ChessPieceIsWhite);
 }
 
 void board_get_moves(Board _board, int pos, Move** moves, size_t* nmoves)
 {
-  size_t idx = 0, current_alloc = 64;
-  *moves = malloc(current_alloc * sizeof(Move));
+  size_t idx = 0;
+  *moves = malloc(64 * sizeof(Move)); // Can be at most 64 moves
 
   u8 pos_88 = topos88(pos);
 
@@ -88,8 +76,9 @@ void board_get_moves(Board _board, int pos, Move** moves, size_t* nmoves)
 
   ChessPiece* board = _board.state;
 
-#define AddMove(move) add_to_moves(moves, move, &current_alloc, &idx)
-#define CanMoveTo(__pos) board_can_move(board[pos], _board, __pos)
+  // Want to check:
+  //  If we're off the board
+  //  We aren't doing a self capture
 
   if (board[pos] & ChessPieceKnight)
   {
@@ -97,59 +86,71 @@ void board_get_moves(Board _board, int pos, Move** moves, size_t* nmoves)
     int move_offsets[] = {14, 18, -14, -18, 33, 31, -33, -31};
     for (int i = 0; i < 8; i++)
     {
-      int tpos_88 = topos88(pos) + move_offsets[i];
+      int tpos_88 = pos_88 + move_offsets[i];
       if (tpos_88 & 0x88)
         continue;
-      if (CanMoveTo(topos64(tpos_88)))
-        AddMove(move_new(pos, topos64(tpos_88)));
+      if (board_can_capture_or_move(_board, pos, topos64(tpos_88)))
+        (*moves)[idx++] = move_new(pos, topos64(tpos_88));
     }
   }
 
   if (board[pos] & ChessPiecePawn)
   {
     bool isWhite = board[pos] & ChessPieceIsWhite;
-    if (isWhite && torank64(pos) == 6 && board[pos - 16] == ChessPieceNone)
-    {
-      AddMove(move_new(pos, pos - 16));
-    }
-    if (!isWhite && torank64(pos) == 1 && board[pos + 16] == ChessPieceNone)
-    {
-      AddMove(move_new(pos, pos + 16));
-    }
     int dirsgn = isWhite ? -1 : 1; // Pawns can only move forwards
-    int tpos = pos + dirsgn * 8;
-    if (!(topos88(tpos) & 0x88) &&
-        !board[tpos]) // Can only move forwards to empty square
-    {
-      AddMove(move_new(pos, pos + dirsgn * 8));
-    }
-    int diagonals[] = {7, 9};
+
+    // Pawns can double move at the start
+    int double_move_rank = isWhite ? 6 : 1;
+    if (torank64(pos) == double_move_rank &&
+        board[pos + dirsgn * 16] == ChessPieceNone)
+      (*moves)[idx++] = move_new(pos, pos - 16);
+
+    int tpos_88 = pos_88 + dirsgn * 16;
+
+    // Pawns can only move forwards to an empty square
+    if (!(tpos_88 & 0x88) && board[topos64(tpos_88)] == ChessPieceNone)
+      (*moves)[idx++] = move_new(pos, topos64(tpos_88));
+
+    int diagonals[] = {15, 17};
     for (int i = 0; i < 2; i++)
     {
-      int tpos = pos + dirsgn * diagonals[i];
-      if (topos88(tpos) & 0x88)
+      int tpos_88 = pos_88 + dirsgn * diagonals[i];
+      int tpos = topos64(tpos);
+      if (tpos_88 & 0x88)
         continue;
-      bool canCapture = isWhite && !(board[tpos] & ChessPieceIsWhite);
-      canCapture = canCapture || !isWhite && (board[tpos] & ChessPieceIsWhite);
-      canCapture = canCapture && board[tpos];
-      if (canCapture)
-      {
-        AddMove(move_new(pos, pos + dirsgn * diagonals[i]));
-      }
+
+      // Pawns can only move diagonally if they're capturing a piece
+      bool can_capture =
+          board_can_capture_or_move(_board, pos, tpos) && board[tpos];
+
+      if (can_capture)
+        (*moves)[idx++] = move_new(pos, tpos);
     }
   }
 
-  if (board[pos] & (ChessPieceBishop | ChessPieceQueen | ChessPieceKing))
+  // Sliding moves (no castling)
+  if (board[pos] &
+      (ChessPieceBishop | ChessPieceCastle | ChessPieceQueen | ChessPieceKing))
   {
-    int slide_offset[] = {17, 15, -17, -15};
-    for (int i = 0; i < 4; i++)
+    int slide_offset[] = {-16, 16, -1, 1, 17, 15, -17, -15};
+
+    int start = 0, end = 8;
+    if (board[pos] & ChessPieceBishop)
+      start = 4;
+    else if (board[pos] & ChessPieceCastle)
+      end = 4;
+
+    for (int i = start; i < end; i++)
     {
       int tpos_88 = topos88(pos) + slide_offset[i];
-      while (!(tpos_88 & 0x88) && CanMoveTo(topos64(tpos_88)))
+      while (!(tpos_88 & 0x88) &&
+             board_can_capture_or_move(_board, pos, topos64(tpos_88)))
       {
-        AddMove(move_new(pos, topos64(tpos_88)));
+        (*moves)[idx++] = move_new(pos, topos64(tpos_88));
+
         if (board[topos64(tpos_88)])
           break;
+
         tpos_88 += slide_offset[i];
         if (board[pos] & ChessPieceKing)
           break;
@@ -157,28 +158,10 @@ void board_get_moves(Board _board, int pos, Move** moves, size_t* nmoves)
     }
   }
 
-  if (board[pos] & (ChessPieceCastle | ChessPieceQueen | ChessPieceKing))
-  {
-    int slide_offset[] = {-16, 16, -1, 1};
-    for (int i = 0; i < 4; i++)
-    {
-      int tpos_88 = topos88(pos) + slide_offset[i];
-      while (!(tpos_88 & 0x88) && CanMoveTo(topos64(tpos_88)))
-      {
-        AddMove(move_new(pos, topos64(tpos_88)));
-        if (board[topos64(tpos_88)])
-          break;
-        tpos_88 += slide_offset[i];
-        if (board[pos] & ChessPieceKing)
-          break;
-      }
-    }
-  }
+  // @@Implement Castling
 
 end:
   *nmoves = idx;
-#undef CanMoveTo
-#undef AddMove
 }
 
 void board_get_moves_all(Board board, Move** moves, size_t* nmoves, int flags)
