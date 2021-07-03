e782cae04e5beddfc8d58071bd784c20a0a09ea5
rleathart <56587304+rleathart@users.noreply.github.com>
2021-07-04 00:03:32 +0100

    Remove duplicate macros

--- include/chess/logging.h
+++ include/chess/logging.h
@@ -15,9 +15,4 @@ extern _Thread_local DebugLevel t_debug_level;
 #define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
 #define DPRINT(fmt, ...) chess_logger(DebugLevelDebug, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
 
-#define WLOG(fmt, ...) chess_logger(DebugLevelWarning, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
-#define ELOG(fmt, ...) chess_logger(DebugLevelError, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
-#define WPRINT(fmt, ...) chess_logger(DebugLevelWarning, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
-#define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
-
 void chess_logger(DebugLevel level, char* file, int line, const char* func, const char* fmt, ...);
