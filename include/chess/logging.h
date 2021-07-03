76a73984aafba227a04548f76973ca928d7aac96
rleathart <56587304+rleathart@users.noreply.github.com>
2021-07-03 22:20:22 +0100

    Fix logging macros on unix

--- include/chess/logging.h
+++ include/chess/logging.h
@@ -6,18 +6,18 @@
 
 extern _Thread_local DebugLevel t_debug_level;
 
-#define ILOG(fmt, ...) chess_logger(DebugLevelInfo, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define WLOG(fmt, ...) chess_logger(DebugLevelWarning, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define ELOG(fmt, ...) chess_logger(DebugLevelError, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define DLOG(fmt, ...) chess_logger(DebugLevelDebug, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define IPRINT(fmt, ...) chess_logger(DebugLevelInfo, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define WPRINT(fmt, ...) chess_logger(DebugLevelWarning, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define DPRINT(fmt, ...) chess_logger(DebugLevelDebug, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
+#define ILOG(fmt, ...) chess_logger(DebugLevelInfo, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define WLOG(fmt, ...) chess_logger(DebugLevelWarning, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define ELOG(fmt, ...) chess_logger(DebugLevelError, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define DLOG(fmt, ...) chess_logger(DebugLevelDebug, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define IPRINT(fmt, ...) chess_logger(DebugLevelInfo, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define WPRINT(fmt, ...) chess_logger(DebugLevelWarning, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define DPRINT(fmt, ...) chess_logger(DebugLevelDebug, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
 
-#define WLOG(fmt, ...) chess_logger(DebugLevelWarning, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define ELOG(fmt, ...) chess_logger(DebugLevelError, __FILE__, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define WPRINT(fmt, ...) chess_logger(DebugLevelWarning, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
-#define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, __VA_ARGS__)
+#define WLOG(fmt, ...) chess_logger(DebugLevelWarning, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define ELOG(fmt, ...) chess_logger(DebugLevelError, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define WPRINT(fmt, ...) chess_logger(DebugLevelWarning, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define EPRINT(fmt, ...) chess_logger(DebugLevelError, NULL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
 
 void chess_logger(DebugLevel level, char* file, int line, const char* func, const char* fmt, ...);
